7c58a02340e45f201fc22dbd2648297fce408c88
Gian Merlino <gianmerlino@gmail.com>
2012-12-18 13:24:11 -0800

    IndexMerger: Failing dimension-order tests

--- server/src/test/java/com/metamx/druid/index/v1/IndexMergerTest.java
+++ server/src/test/java/com/metamx/druid/index/v1/IndexMergerTest.java
@@ -19,18 +19,22 @@
 
 package com.metamx.druid.index.v1;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import com.metamx.druid.QueryGranularity;
 import com.metamx.druid.aggregation.AggregatorFactory;
+import com.metamx.druid.input.InputRow;
 import com.metamx.druid.input.MapBasedInputRow;
 import junit.framework.Assert;
 import org.apache.commons.io.FileUtils;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  */
@@ -59,9 +63,8 @@ public class IndexMergerTest
   public void testPersistMergeCaseInsensitive() throws Exception
   {
     final long timestamp = System.currentTimeMillis();
-    IncrementalIndex toPersist1 = IncrementalIndexTest.createCaseInsensitiveIndex(timestamp);
-
-    IncrementalIndex toPersist2 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
+    final IncrementalIndex toPersist1 = IncrementalIndexTest.createCaseInsensitiveIndex(timestamp);
+    final IncrementalIndex toPersist2 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
 
     toPersist2.add(
         new MapBasedInputRow(
@@ -102,6 +105,125 @@ public class IndexMergerTest
 
       Assert.assertEquals(3, merged.getTimestamps().size());
       Assert.assertEquals(Arrays.asList("dim1", "dim2"), Lists.newArrayList(merged.getAvailableDimensions()));
+      Assert.assertEquals(0, merged.getAvailableMetrics().size());
+    }
+    finally {
+      FileUtils.deleteQuietly(tempDir1);
+      FileUtils.deleteQuietly(tempDir2);
+      FileUtils.deleteQuietly(mergedDir);
+    }
+  }
+
+  @Test
+  public void testPersistMergeNonLexicographicDims() throws Exception
+  {
+    final long timestamp = new DateTime("2000-01-01").getMillis();
+    final IncrementalIndex toPersist1 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
+    final IncrementalIndex toPersist2 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
+
+    final List<String> dimensions = ImmutableList.of("b", "a");
+
+    final InputRow row1 = new MapBasedInputRow(
+        timestamp,
+        dimensions,
+        ImmutableMap.<String, Object>of(
+            "a", "x",
+            "b", "y"
+        )
+    );
+
+    final InputRow row2 = new MapBasedInputRow(
+        timestamp,
+        dimensions,
+        ImmutableMap.<String, Object>of(
+            "a", "y",
+            "b", "x"
+        )
+    );
+
+    toPersist1.add(row1);
+    toPersist1.add(row2);
+    toPersist2.add(row1);
+    toPersist2.add(row2);
+
+    final File tempDir1 = Files.createTempDir();
+    final File tempDir2 = Files.createTempDir();
+    final File mergedDir = Files.createTempDir();
+    try {
+      MMappedIndex index1 = IndexIO.mapDir(IndexMerger.persist(toPersist1, tempDir1));
+
+      Assert.assertEquals("1.size", 2, index1.getTimestamps().size());
+      Assert.assertEquals(0, index1.getAvailableMetrics().size());
+
+      MMappedIndex index2 = IndexIO.mapDir(IndexMerger.persist(toPersist2, tempDir2));
+
+      Assert.assertEquals("2.size", 2, index2.getTimestamps().size());
+      Assert.assertEquals(0, index2.getAvailableMetrics().size());
+
+      MMappedIndex merged = IndexIO.mapDir(
+          IndexMerger.mergeMMapped(Arrays.asList(index1, index2), new AggregatorFactory[]{}, mergedDir)
+      );
+
+      Assert.assertEquals("merged.size", 2, merged.getTimestamps().size());
+      Assert.assertEquals(dimensions, Lists.newArrayList(merged.getAvailableDimensions()));
+      Assert.assertEquals(0, merged.getAvailableMetrics().size());
+    }
+    finally {
+      FileUtils.deleteQuietly(tempDir1);
+      FileUtils.deleteQuietly(tempDir2);
+      FileUtils.deleteQuietly(mergedDir);
+    }
+  }
+
+  @Test
+  public void testPersistMergeDifferentDims() throws Exception
+  {
+    final long timestamp = new DateTime("2000-01-01").getMillis();
+    final IncrementalIndex toPersist1 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
+    final IncrementalIndex toPersist2 = new IncrementalIndex(0L, QueryGranularity.NONE, new AggregatorFactory[]{});
+
+    final InputRow row1 = new MapBasedInputRow(
+        timestamp,
+        ImmutableList.of("a"),
+        ImmutableMap.<String, Object>of(
+            "a", "y"
+        )
+    );
+
+    final InputRow row2 = new MapBasedInputRow(
+        timestamp,
+        ImmutableList.of("a", "b"),
+        ImmutableMap.<String, Object>of(
+            "a", "x",
+            "b", "y"
+        )
+    );
+
+    toPersist1.add(row1);
+    toPersist1.add(row2);
+    toPersist2.add(row1);
+    toPersist2.add(row2);
+
+    final File tempDir1 = Files.createTempDir();
+    final File tempDir2 = Files.createTempDir();
+    final File mergedDir = Files.createTempDir();
+    try {
+      MMappedIndex index1 = IndexIO.mapDir(IndexMerger.persist(toPersist1, tempDir1));
+
+      Assert.assertEquals("1.size", 2, index1.getTimestamps().size());
+      Assert.assertEquals(0, index1.getAvailableMetrics().size());
+
+      MMappedIndex index2 = IndexIO.mapDir(IndexMerger.persist(toPersist2, tempDir2));
+
+      Assert.assertEquals("2.size", 2, index2.getTimestamps().size());
+      Assert.assertEquals(0, index2.getAvailableMetrics().size());
+
+      MMappedIndex merged = IndexIO.mapDir(
+          IndexMerger.mergeMMapped(Arrays.asList(index1, index2), new AggregatorFactory[]{}, mergedDir)
+      );
+
+      Assert.assertEquals("merged.size", 2, merged.getTimestamps().size());
+      Assert.assertEquals(Lists.newArrayList("a", "b"), Lists.newArrayList(merged.getAvailableDimensions()));
       Assert.assertEquals(0, merged.getAvailableMetrics().size());
     }
     finally {
